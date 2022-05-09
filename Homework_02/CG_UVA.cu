69499f18e82879463eb597eb45450925e882db70
Su kezheng <73925990+ssskz@users.noreply.github.com>
2022-05-09 09:14:50 +0800

    Update and rename UM.cu to CG_UVA.cu

---
+++ Homework_02/CG_UVA.cu
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<cuda_runtime.h>
+#define BLOCKNUM 16384
+#define THREADNUM 512
+
+__global__ void Ginit(int *a,int *b)
+{
+    int tid=blockDim.x*blockIdx.x+threadIdx.x;
+    a[tid]=blockIdx.x;
+    b[tid]=blockIdx.x+1;   
+}
+
+__global__ void Gmultiply(int* a,int *b)
+{
+    int tid=blockDim.x*blockIdx.x+threadIdx.x;
+    a[tid]=a[tid]*b[tid];
+} 
+
+void CpuMul(int*a,int *b)
+{
+    for(int i=0;i<BLOCKNUM;i++)
+	{
+        for(int j=0;j<THREADNUM;j++)
+		{
+            int tid=i*THREADNUM+j;
+            a[tid]=j;
+            b[tid]=j+1;
+            a[tid]=a[tid]*b[tid];
+        }
+    }
+	printf("CPU Multiply Finished.\n");
+}
+
+int main()
+{
+    int* a;
+    int* b;
+    cudaSetDevice(1);
+    cudaMallocManaged(&a,BLOCKNUM*THREADNUM*sizeof(int));
+    cudaMallocManaged(&b,BLOCKNUM*THREADNUM*sizeof(int));
+    CpuMul(a,b);
+    cudaMemPrefetchAsync (a,BLOCKNUM*THREADNUM*sizeof(int), 1); 
+    cudaMemPrefetchAsync (b,BLOCKNUM*THREADNUM*sizeof(int), 1); 
+    Ginit<<<BLOCKNUM,THREADNUM>>>(a,b);
+	printf("GPU Initialization Finished.\n");
+    cudaDeviceSynchronize();
+    Gmultiply<<<BLOCKNUM,THREADNUM>>>(a,b);
+	printf("GPU Multiply Finished.\n");
+    cudaDeviceSynchronize();
+    cudaFree(a);
+    cudaFree(b);
+    return 0;
+} 
